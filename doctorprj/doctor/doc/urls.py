585962f34851923a5b92fde2a5b093c9189ca958
somesh-py <someshsharma615@gmail.com>
2023-02-27 10:15:18 +0530

    add image with image procedure

--- doctorprj/doctor/doc/urls.py
+++ doctorprj/doctor/doc/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    path('',views.index),
-    path('registration/',views.registration),
-    path('login/',views.login),
-    path('logindata/',views.logindata),
-]
+    path('', views.index),
+    path('registration/', views.registration),
+    path('login/', views.login),
+    path('logindata/', views.logindata),
+    path('table/',views.table),
+    path('update/<int:tid>/',views.update),
+    path('updatedata/',views.updatedata),
+    path('delete/<int:tid>/',views.delete),
+    
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- doctorprj/doctor/doc/views.py
+++ doctorprj/doctor/doc/views.py
@@ -1,52 +1,87 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth.hashers import make_password,check_password
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password, check_password
 from .models import Doctor
 from django.contrib import messages
 # Create your views here.
 
+
 def index(request):
     return render(request,'index.html')
 
+
 def registration(request):
-    if request.method=='POST':
-        name=request.POST['name']
-        degree=request.POST['degree']
-        contact=request.POST['contact']
-        email=request.POST['email']
-        password=make_password(request.POST['password'])
-        image=request.POST['image']
-        catagory=request.POST['catagory']
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        degree = request.POST.get('degree')
+        contact = request.POST.get('contact')
+        email = request.POST.get('email')
+        password = make_password(request.POST.get('password'))
+        imaged = request.FILES.get('imaged')
+        catagory = request.POST.get('catagory')
         if Doctor.objects.filter(email=email).exists():
-            messages.error(request,'email already exist')
+            messages.error(request, 'email already exist')
             return redirect('/')
         elif Doctor.objects.filter(contact=contact).exists():
-            messages.error(request,'contatc number already exist')
+            messages.error(request, 'contatc number already exist')
             return redirect('/')
         else:
-            Doctor.objects.create(name=name,degree=degree,contact=contact,email=email,password=password,image=image,catagory=catagory)
-            messages.success(request,'registration done sucessfully')
+            Doctor.objects.create(name=name, degree=degree, contact=contact,
+                                  email=email, password=password, image=imaged, catagory=catagory)
+            messages.success(request, 'registration done sucessfully')
             return redirect('/login/')
-        
+
+
 def login(request):
-    return render(request,'login.html')
+    return render(request, 'login.html')
+
+
 def logindata(request):
-    if request.method=='POST':
-        contact=request.POST['contact']
-        email=request.POST['email']
-        password=request.POST['password']
+    if request.method == 'POST':
+        contact = request.POST['contact']
+        email = request.POST['email']
+        password = request.POST['password']
         if Doctor.objects.filter(email=email).exists():
             if Doctor.objects.filter(contact=contact).exists():
-                i_email=Doctor.objects.get(email=email)
-                i_password=i_email.password
-                if check_password(password,i_password):
-                    messages.success(request,'loginsucessfully')
-                    return redirect('/login/')
+                i_email = Doctor.objects.get(email=email)
+                i_password = i_email.password
+                if check_password(password, i_password):
+                    messages.success(request, 'loginsucessfully')
+                    return redirect('/table/')
                 else:
-                    messages.error(request,'password was incorrect')
+                    messages.error(request, 'password was incorrect')
                     return redirect('/login/')
             else:
-                messages.error(request,'contact error')
+                messages.error(request, 'contact error')
                 return redirect('/login/')
         else:
-            messages.error(request,'email not exist put correct email')
+            messages.error(request, 'email not exist put correct email')
             return redirect('/login/')
+
+
+def table(request):
+    doctordata = Doctor.objects.all()
+    return render(request, 'table.html', {'ddata': doctordata})
+
+
+def update(request, tid):
+    tid = Doctor.objects.get(id=tid)
+    return render(request, 'update.html', {'id': tid})
+
+
+def updatedata(request):
+    if request.method == 'POST':
+        tid = request.POST.get('tid')
+        name = request.POST.get('name')
+        degree = request.POST.get('degree')
+        contact = request.POST.get('contact')
+        password = make_password(request.POST.get('password'))
+        email = request.POST.get('email')
+        catagory = request.POST.get('catagory')
+        image=request.FILES.get('image')
+        if Doctor.objects.filter(id=tid).exists():
+            Doctor.objects.update(name=name,degree=degree,contact=contact,password=password,email=email,catagory=catagory,image=image)
+            return redirect('/table/')
+            
+def delete(request,tid):
+    Doctor.objects.filter(id=tid).delete()
+    return redirect('/table/')
